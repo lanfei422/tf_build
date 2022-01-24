04c1d87ed5b17f58be773d49a76e54085b62a40f
Yanlf <492254319@qq.com>
2022-01-24 14:06:47 +0800

    add image name

--- build.sh
+++ build.sh
@@ -2,23 +2,25 @@
 
 func() {
   echo "Usage:"
-  echo "build.sh [-p port] [-g with_gpu = true|false]"
+  echo "build.sh [-p port] [-g with_gpu = true|false] -n [img_name]"
   echo "Description:"
   echo "port, the web port to be exposed."
   echo "with_gpu, with_gpu weather to use gpu."
+  echo "img_name "
   exit 1
 }
 
-while getopts 'p:g:' OPT; do
+while getopts 'p:g:n:' OPT; do
   case $OPT in
   p) port="$OPTARG" ;;
   g) with_gpu="$OPTARG" ;;
+  n) img_name="$OPTARG" ;;
   ?) func ;;
   esac
 done
 
 if [ $with_gpu == "true" ]; then
-  docker build -t --build-arg web_port=${port} -f ./Dockerfile.gpu .;
+  docker build -t ${img_name} --build-arg web_port=${port} -f ./Dockerfile.gpu .;
 else
-  docker build -t --build-arg web_port=${port} -f ./Dockerfile .;
+  docker build -t ${img_name} --build-arg web_port=${port} -f ./Dockerfile .;
 fi
